d4a4fcca022d02361c87255f409639176eeb0678
farizashakir1309 <farizashkair13@gmail.com>
2020-09-20 06:25:47 +0000

    initial

---
+++ server.js
@@ -0,0 +1,29 @@
+var express=require("express");
+var app=express();
+var ejs=require("ejs");
+var path=require("path");
+var expressLayout=require("express-ejs-layouts");
+var PORT=process.env.PORT || 3300;
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+
+
+
+
+//set templte engine
+app.use(expressLayout);
+ app.set("views",path.join(__dirname,"/resources/views"));
+app.set("view engine","ejs");
+
+
+
+
+
+
+
+
+app.listen(PORT,function(){
+	console.log("Listening on port 3300");
+})
