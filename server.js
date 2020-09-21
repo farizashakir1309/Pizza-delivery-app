af9d7a47efb997f2fc4fc7a0a6f825416b5ecad8
farizashakir1309 <farizashkair13@gmail.com>
2020-09-21 16:26:18 +0000

    add cart page and auth

--- server.js
+++ server.js
@@ -4,18 +4,10 @@ var ejs=require("ejs");
 var path=require("path");
 var expressLayout=require("express-ejs-layouts");
 var PORT=process.env.PORT || 3300;
-
-
 app.use(express.static("public"));
-app.get("/",function(req,res){
-	res.render("home");
-});
 
 
 
-
-
-//set templte engine
 app.use(expressLayout);
  app.set("views",path.join(__dirname,"/resources/views"));
 app.set("view engine","ejs");
@@ -23,6 +15,29 @@ app.set("view engine","ejs");
 
 
 
+
+
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+ app.get("/cart",function(req,res){
+	 res.render("customers/cart");
+ });
+
+app.get("/login",function(req,res){
+	res.render("auth/login");
+});
+app.get("/register",function(req,res){
+	res.render("auth/register");
+});
+
+
+
+
+
+//set templte engine
 
 
 
